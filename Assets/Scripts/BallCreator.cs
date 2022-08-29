b7478d2d7858bd9839d0743cf868046ee574576d
junghyunhwang <dmagk560@gmail.com>
2022-08-29 15:07:38 +0900

    Fix standard and Fix get Ball position

--- Assets/Scripts/BallCreator.cs
+++ Assets/Scripts/BallCreator.cs
@@ -4,31 +4,22 @@ using UnityEngine;
 
 public class BallCreator : MonoBehaviour
 {
-    [SerializeField] private GameObject _BBall;
-    [SerializeField] private GameObject _RBall;
+    [SerializeField] private GameObject RedBall;
+    [SerializeField] private GameObject BlueBall;
 
     void Start()
     {
-        /*GameObject ball = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-
-        ball.transform.position = new Vector3(0, 3, 0);
-
-        Rigidbody rb = ball.AddComponent<Rigidbody>();
-        rb.mass = 0.2f;
-
-        ball.AddComponent<PlayerBall>();*/
-
         CreateBall();
     }
 
     private void CreateBall()
     {
-        GameObject ball1 = Instantiate(_RBall);
-        GameObject ball2 = Instantiate(_BBall);
+        GameObject redBall = Instantiate(RedBall);
+        GameObject blueBall = Instantiate(BlueBall);
 
         Vector2 spawnPoint = new Vector3(3.5f, 3f);
 
-        ball1.transform.position = spawnPoint;
-        ball2.transform.position = spawnPoint * new Vector2(-1f, 1f);
+        redBall.transform.position = spawnPoint;
+        blueBall.transform.position = spawnPoint * new Vector2(-1f, 1f);
     }
 }
--- Assets/Scripts/PlayerBall.cs
+++ Assets/Scripts/PlayerBall.cs
@@ -5,36 +5,34 @@ using UnityEngine.UI;
 
 public class PlayerBall : MonoBehaviour
 {
-    [SerializeField] private Text _RestartTxT;
+    enum EBallColor
+    {
+        Blue,
+        Red
+    }
 
-    private Rigidbody rb;
-
-    private Vector3 _MoveDrection;
-
-    // 공 상태
-    private enum PlayerState { Blue, Red}
-    [SerializeField] PlayerState _PlayerState;
-
-    private float _DrectionX = 0.0f;
-    private float _DrectionZ = 0.0f;
+    [SerializeField] private Text RestartTxT;
+    private Rigidbody Rb;
+    private Vector3 MoveDrection;
+    [SerializeField] EBallColor PlayerColor;
+    private float DrectionX = 0.0f;
+    private float DrectionZ = 0.0f;
 
     private void Awake()
     {
-        rb = GetComponent<Rigidbody>();
+        Rb = GetComponent<Rigidbody>();
 
-        _RestartTxT = GameObject.Find("RestartTxT").GetComponent<Text>();
+        RestartTxT = GameObject.Find("RestartTxT").GetComponent<Text>();
     }
 
     void Start()
-    {  
-        _RestartTxT.gameObject.SetActive(false);
+    {
+        RestartTxT.gameObject.SetActive(false);
     }
 
     void Update()
     {
-        Vector3 pos = GetComponent<Transform>().position;
-
-        if (pos.y < -5)
+        if (transform.position.y < -5)
         {
             Debug.Log("Destroy");
             Destroy(gameObject);
@@ -43,90 +41,84 @@ public class PlayerBall : MonoBehaviour
 
     private void FixedUpdate()
     {
-        // 이동 호출
         MoveBall();
-
-        /*float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
-        Vector3 move = new Vector3(horizontal, 0, vertical);
-
-        rb.AddForce(move, ForceMode.Force);*/
     }
 
     private void MoveBall()
     {
-        // 뻘건공
-        if (_PlayerState == PlayerState.Red)
+        if (PlayerColor == EBallColor.Red)
         {
             if (Input.GetKey(KeyCode.RightArrow))
             {
-                _DrectionX = 1f;
+                DrectionX = 1f;
             }
-
             else if (Input.GetKey(KeyCode.LeftArrow))
             {
-                _DrectionX = -1f;
+                DrectionX = -1f;
             }
-
-            else { _DrectionX = 0.0f; }
+            else
+            {
+                DrectionX = 0.0f;
+            }
 
             if (Input.GetKey(KeyCode.UpArrow))
             {
-                _DrectionZ = 1f;
+                DrectionZ = 1f;
             }
-
             else if (Input.GetKey(KeyCode.DownArrow))
             {
-                _DrectionZ = -1f;
+                DrectionZ = -1f;
             }
-
-            else { _DrectionZ = 0.0f; }
+            else
+            {
+                DrectionZ = 0.0f;
+            }
         }
 
-        // 퍼런공
-        if (_PlayerState == PlayerState.Blue)
+        if (PlayerColor == EBallColor.Blue)
         {
             if (Input.GetKey(KeyCode.D))
             {
-                _DrectionX = 1f;
+                DrectionX = 1f;
             }
-
             else if (Input.GetKey(KeyCode.A))
             {
-                _DrectionX = -1f;
+                DrectionX = -1f;
             }
-
-            else { _DrectionX = 0.0f; }
+            else
+            {
+                DrectionX = 0.0f;
+            }
 
             if (Input.GetKey(KeyCode.W))
             {
-                _DrectionZ = 1f;
+                DrectionZ = 1f;
             }
-
             else if (Input.GetKey(KeyCode.S))
             {
-                _DrectionZ = -1f;
+                DrectionZ = -1f;
             }
-
-            else { _DrectionZ = 0.0f; }
+            else
+            {
+                DrectionZ = 0.0f;
+            }
         }
 
-        _MoveDrection = new Vector3(_DrectionX, 0.0f, _DrectionZ);
+        MoveDrection = new Vector3(DrectionX, 0.0f, DrectionZ);
 
-        // 이동
-        rb.AddForce(_MoveDrection, ForceMode.Force);
+        Rb.AddForce(MoveDrection, ForceMode.Force);
     }
 
     private void OnDisable()
     {
-        _RestartTxT.gameObject.SetActive(true);
+        RestartTxT.gameObject.SetActive(true);
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.transform.tag == "Player")
         {
-            Debug.Log("닿음");
+            Debug.Log("????");
         }
     }
 }
